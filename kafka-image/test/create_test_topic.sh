291b69f50792882f29b8a2857afcbe7bd7cf28b3
RichardHightower <richard.m.hightower@gmail.com>
2018-02-02 15:39:13 -0800

    added new topic creation lockdown

--- kafka-image/test/create_test_topic.sh
+++ kafka-image/test/create_test_topic.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 opt/kafka/bin/kafka-topics.sh --create \
---zookeeper 0.0.0.0:2181 \
+--zookeeper kafka0:2181 \
 --replication-factor 1 \
 --partitions 9 \
 --topic test-topic
