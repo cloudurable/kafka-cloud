f95e5c818f13f707a72934284fbfc1cdf1aa0cd3
Rick Hightower <richardhightower@gmail.com>
2022-07-20 12:35:56 -0500

    added support for latest kafka on latest OS

--- kafka-image/test/create_test_topic.sh
+++ kafka-image/test/create_test_topic.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 opt/kafka/bin/kafka-topics.sh --create \
---bootstrap-server localhost:9092 \
+--bootstrap-server kafka0:9092 \
 --replication-factor 1 \
 --partitions 9 \
 --topic test-topic
